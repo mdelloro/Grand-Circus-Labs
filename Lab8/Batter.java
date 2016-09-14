555d23ec493906e5f17b96d0a60f3bf8739e3928
mdelloro <m.delloro@yahoo.com>
2016-09-14 11:46:51 -0400

    Lab 8 BatterAvg

---
+++ Lab8/Batter.java
@@ -0,0 +1,45 @@
+package Lab8;
+
+import java.text.DecimalFormat;
+
+public class Batter {
+	private String name;
+	private int basesEarned;	
+
+	public Batter(String name, int basesEarned) {
+		this.name = name;
+		this.setBasesEarned(basesEarned);
+	}
+	
+	public String getBattingAvg(int atLeastOne, int atBats){
+		double value = atLeastOne/(double)atBats;
+		return formatNumber(value);
+	}
+
+	public String formatNumber(double value){
+		DecimalFormat df = new DecimalFormat(".###");
+		return df.format(value);
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getBasesEarned() {
+		return basesEarned;
+	}
+
+	public void setBasesEarned(int basesEarned) {
+		this.basesEarned = basesEarned;
+	}
+
+	@Override
+	public String toString() {
+		return "Batter [name=" + name + ", basesEarned=" + basesEarned + "]";
+	}
+	
+}
